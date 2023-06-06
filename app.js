c82044cac9fd6db577c6cabd1ebfb2b2062513c2
Aurelio Killian Lexi Verrill <aurelioklv@gmail.com>
2023-06-06 18:50:33 +0700

    feat: update account endpoint

--- app.js
+++ app.js
@@ -3,29 +3,61 @@ const app = express();
 const cors = require("cors");
 const { db } = require("./db");
 const session = require("express-session");
+const store = new session.MemoryStore();
 const port = 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: "rahasiabanget",
     cookie: {
-      sameSite: "strict",
+      maxAge: 30000,
     },
+    resave: false,
+    saveUninitialized: false,
+    store,
   })
 );
 
+// app.use((req, res, next) => {
+//   console.log(store);
+//   next();
+// });
+
 const acc_ktp = 3504098213010001;
 
+app.use(express.static("public"));
+
 app.get("/account", async (req, res) => {
+  const { email, ktp } = req.query;
   try {
-    const acc = await db.query("SELECT * FROM ACCOUNT WHERE ACC_KTP_NUM = $1", [
-      acc_ktp,
-    ]);
-    res.json(acc.rows);
+    if (email) {
+      const acc = await db.query("SELECT * FROM ACCOUNT WHERE ACC_EMAIL = $1", [
+        email,
+      ]);
+      if (acc.rows.length === 0) {
+        return res.json({ email_exists: false }); // Account not found
+      }
+      res.json({ email_exists: true });
+    } else if (ktp) {
+      const acc = await db.query(
+        "SELECT * FROM ACCOUNT WHERE ACC_KTP_NUM = $1",
+        [ktp]
+      );
+      if (acc.rows.length === 0) {
+        return res.json({ ktp_exists: false }); // Account not found
+      }
+      res.json({ ktp_exists: true });
+    } else {
+      const acc = await db.query(
+        "SELECT * FROM ACCOUNT WHERE ACC_KTP_NUM = $1",
+        [acc_ktp]
+      );
+      res.json(acc.rows);
+    }
   } catch (err) {
     console.log(err.message);
   }
@@ -33,6 +65,48 @@ app.get("/account", async (req, res) => {
 
 app.post("/account", async (req, res) => {
   try {
+    const data = req.body;
+    const new_acc = await db.query(
+      `INSERT INTO ACCOUNT(
+          ACC_KTP_NUM,
+          ACC_AT_ID,
+          ACC_NAME,
+          ACC_EMAIL,
+          ACC_PHONE_NUM,
+          ACC_ADDRESS,
+          ACC_BANK_NUM,
+          ACC_BANK,
+          ACC_JOB,
+          ACC_SALARY,
+          ACC_REG_DATE,
+          ACC_EMERGENCY_PHONE_NUM,
+          ACC_EMERGENCY_CONTACT_RELATIONSHIP,
+          ACC_RATING,
+          ACC_BALANCE,
+          ACC_BIOLOGICAL_MOTHER_NAME,
+          ACC_PASSWORD
+        )
+        VALUES (
+          $1, NULL, $2, $3, $4, $5, $6, $7, $8, $9, NOW(),  $10, $11, DEFAULT, NULL, $12, $13
+        )`,
+      [
+        data.ktp,
+        data.name,
+        data.email,
+        data.phone,
+        data.address,
+        data.bank_num,
+        data.bank,
+        data.job,
+        data.salary,
+        data.emergency_contact,
+        data.emergency_contact_rel,
+        data.mother,
+        data.password,
+      ]
+    );
+    res.redirect("login.html");
+    // res.sendStatus(200).json({ message: "Terdaftar" });
   } catch (err) {
     console.log(err.message);
   }
